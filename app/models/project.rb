1b4321c36d38b7580d05ad3976b33345343731ae
Kevin Fischer <kevin@agileware.jp>
2021-02-13 14:32:03 +0900

    Don't allow publish without permission

--- app/models/project.rb
+++ app/models/project.rb
@@ -818,7 +818,6 @@ class Project < ActiveRecord::Base
     'name',
     'description',
     'homepage',
-    'is_public',
     'identifier',
     'custom_field_values',
     'custom_fields',
@@ -844,6 +843,12 @@ class Project < ActiveRecord::Base
         end
       end
   )
+
+  safe_attributes(
+    'is_public',
+    :if => lambda do |project, user|
+      project.is_public? || user.allowed_to?(:publish_project, project)
+    end)
 
   safe_attributes(
     'inherit_members',
