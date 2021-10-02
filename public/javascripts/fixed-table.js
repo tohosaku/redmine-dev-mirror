bb2517b19b01f380566962179136a6613a886a3b
tohosaku <tohosaku@users.osdn.me>
2021-10-02 07:16:13 +0000

    rewrite to class

--- public/javascripts/fixed-table.js
+++ public/javascripts/fixed-table.js
@@ -1,96 +1,133 @@
+function toNum(width) {
+    return (width.replace(/px/g, '') * 1);
+}
+
+class StyleInfo {
+
+    constructor(element) {
+        this.element = element;
+    }
+
+    get computedStyle() {
+        return getComputedStyle(this.element);
+    }
+
+    get style() {
+        return this.element.style;
+    }
+
+    setWidth(clientWidth) {
+        const width = clientWidth - this.getPropertyValue('padding-left') - this.getPropertyValue('padding-right')
+        this.style.width = `${width}px`;
+    }
+
+    clearWidth() {
+        this.style.width = '';
+    }
+
+    getPropertyValue(key) {
+        return toNum(this.computedStyle.getPropertyValue(key));
+    }
+}
+
+class ColumnStyles {
+
+    constructor(headers, items) {
+        this.columns = items.map((item, i) => 
+        ({
+            head: new StyleInfo(headers[i]),
+            item: new StyleInfo(item)
+        }));
+    }
+    
+    setColumnWidth() {
+        this.columns.forEach(c => {
+            const clientwidth = c.item.element.clientWidth;
+            c.item.setWidth(clientwidth);
+            c.head.setWidth(clientwidth);
+        });
+    }
+    
+    clear() {
+        this.columns.forEach(c => {
+            c.item.clearWidth();
+            c.head.clearWidth();
+        })
+    }
+
+    reset() {
+        this.clear();
+        this.setColumnWidth();
+    }
+}
+
+class PositionInfo {
+
+    constructor(table, headerrow, itemrow) {
+        this.table = table;
+        this.headerrow = headerrow;
+        this.itemrow = itemrow
+    }
+
+    getPosition() {
+        if (this.rect.top < 0) {
+            return { top: '0', position: 'fixed', left: `${this.getLeft()}px`, zIndex: 1, tbodyTop: `${this.getTbodyTop()}px`, tbodyPosition: 'relative' }
+        } else {
+            return { top: '', position: '', zIndex: '', tbodyTop: '', tbodyPosition: '' }
+        }
+    }
+
+    getTbodyTop() {
+        const hheight = getComputedStyle(this.headerrow).height;
+        return toNum(hheight) + this.rect.top;
+    }
+    
+    getLeft() {
+        return this.rect.left + this.scrollLeft;
+    }
+
+    check() {
+        const pos = this.getPosition();
+        const headerRowStyle = headerrow.style;
+    
+        if (typeof pos.left !== 'undefined' && headerRowStyle.left !== pos.left) {
+            headerRowStyle.left = pos.left;
+        }
+    
+        if (headerRowStyle.top !== pos.top) {
+            headerRowStyle.top = pos.top;
+            headerRowStyle.position = pos.position;
+            headerRowStyle.zIndex = pos.zIndex;
+        }
+    
+        const tbody = this.itemrow.parentElement;
+        if (tbody.style.top !== pos.tbodyTop || toNum(pos.tbodyTop) > 0) {
+            tbody.style.top = pos.tbodyTop;
+            tbody.style.position = pos.tbodyPosition;
+        }
+    }
+
+    get scrollLeft() {
+        return window.pageXOffset;
+    }
+    
+    get rect() {
+        return this.table.getBoundingClientRect();
+    }
+}
+
 const table = document.querySelector('table.issues.list');
 
 const headerrow = table.querySelector('thead tr');
 const firstrow = table.querySelector('tbody tr');
 
-function getColumnPair() {
-    const headers = Array.from(headerrow.children);
-    return Array.from(firstrow.children).map((item, i) => 
-        ({
-            head: {
-                computedStyle: getComputedStyle(headers[i]),
-                style: headers[i].style
-            },
-            item: {
-                computedStyle: getComputedStyle(item),
-                style: item.style
-            },
-            clientWidth: () => item.clientWidth
-        }));
-}
-
-function getWidth(clientWidth, computedstyle) {
-    const width = clientWidth - toNum(computedstyle.getPropertyValue('padding-left')) - toNum(computedstyle.getPropertyValue('padding-right'))
-    return `${width}px`
-}
-
-function toNum(width) {
-    return (width.replace(/px/g, '') * 1);
-}
-
-function setColumnWidth() {
-    columns.forEach(c => {
-        const clientwidth = c.clientWidth();
-        c.item.style.width = getWidth(clientwidth , c.item.computedStyle);
-        c.head.style.width = getWidth(clientwidth , c.head.computedStyle);
-    });
-}
-
-function clear() {
-    columns.forEach(c => {
-        c.item.style.width = '';
-        c.head.style.width = '';
-    })
-}
-
-function getPosition() {
-    const rect = table.getBoundingClientRect();
-
-    if (rect.top < 0) {
-        const hheight = getComputedStyle(headerrow).height;
-        const tbodyTop = toNum(hheight) + rect.top;
-        return { top: '0', position: 'fixed', left: `${getLeft()}px`, zIndex: 1, tbodyTop: `${tbodyTop}px`, tbodyPosition: 'relative' }
-    } else {
-        return { top: '', position: '', zIndex: '', tbodyTop: '', tbodyPosition: '' }
-    }
-}
-
-function getLeft() {
-    const rect = table.getBoundingClientRect();
-    const scrollLeft = window.pageXOffset;
-    return rect.left + scrollLeft;
-}
-
-const columns = getColumnPair();
-
-function toggleFixed() {
-    const pos = getPosition();
-    const headerRowStyle = headerrow.style;
-
-    if (typeof pos.left !== 'undefined' && headerRowStyle.left !== pos.left) {
-        headerRowStyle.left = pos.left;
-    }
-
-    if (headerRowStyle.top !== pos.top) {
-        headerRowStyle.top = pos.top;
-        headerRowStyle.position = pos.position;
-        headerRowStyle.zIndex = pos.zIndex;
-    }
-
-    const tbody = firstrow.parentElement;
-    if (tbody.style.top !== pos.tbodyTop || toNum(pos.tbodyTop) > 0) {
-        tbody.style.top = pos.tbodyTop;
-        tbody.style.position = pos.tbodyPosition;
-    }
-}
-
-function resetTable() {
-    clear();
-    setColumnWidth();
-}
-
 if (table !== null) {
-    setColumnWidth();
-    window.addEventListener('scroll', toggleFixed, { passive: true });
-    window.addEventListener('resize', resetTable, { passive: true });
+
+    const columnStyles = new ColumnStyles(Array.from(headerrow.children), Array.from(firstrow.children));
+    columnStyles.setColumnWidth();
+
+    const posInfo = new PositionInfo(table, headerrow, firstrow);
+
+    window.addEventListener('scroll', () => posInfo.check(), { passive: true });
+    window.addEventListener('resize', () => columnStyles.reset(), { passive: true });
 }
